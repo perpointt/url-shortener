1ca40d9014df61b2aa91839b93e81a8f08d25328
Maxim Ilyshchenko <per.pointt@gmail.com>
2024-10-08 17:53:48 +0300

    feat: init router

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/storage/sqlite"
 )
 
@@ -22,13 +25,19 @@ func main() {
 	log.Debug("debug messages are enabled")
 
 	storage, err := sqlite.New(cfg.StoragePath)
-
 	if err != nil {
 		log.Error("failed to create storage: ", err)
 		os.Exit(1)
 	}
 
 	_ = storage
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 }
 
 func setupLogger(env string) *slog.Logger {
