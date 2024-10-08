80681870681e8e61727489a740385b9b6d28fb75
Maxim Ilyshchenko <per.pointt@gmail.com>
2024-10-08 16:26:16 +0300

    feat: add logs

---
+++ internal/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" envDefault:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTPPServer  `yaml:"http_server"`
+}
+
+type HTPPServer struct {
+	Address     string `yaml:"address" env-default:"localhost:8080"`
+	Timeout     int    `yaml:"timeout" env-default:"4"`
+	IdleTimeout int    `yaml:"idle_timeout" env-default:"60"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file %s does not exist", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	return &cfg
+}
